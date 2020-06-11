e0339826bbf470274100b76c33bf557486117cbf
Shoaib Masood <shoaib_masood@rocketmail.com>
2020-06-12 03:05:27 +0500

    empty card item

--- src/components/Carditem/Carditem.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-// import {Card, CardContent, Typography, Grid} from "@material-ui/core";
-
-const Carditem = (props) => {
-  console.log(props);
-  return <div>{`this is active cases ${props.active}`}</div>;
-};
-export default Carditem;
